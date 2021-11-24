f81bcea06b3772006a1a4ea0d25d4798329e2f4b
[Elena] <[liabaturina@yandex.ru]>
2021-11-24 19:34:11 +0300

    Initial commit.

---
+++ src/geekbrains/lession_hello_world/Main.java
@@ -0,0 +1,9 @@
+package geekbrains.lession_hello_world;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello world!");
+        System.out.println("Я буду изучать Java!");
+    }
+}
